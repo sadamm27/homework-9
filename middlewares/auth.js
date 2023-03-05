c20511ea8db13597fe065970acdbd892d46fb19e
Mochammad Sadam <122198036+sadamm27@users.noreply.github.com>
2023-03-05 14:52:28 +0700

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,54 @@
-const jwt = require("jsonwebtoken");
-
+const jwt = require ("jsonwebtoken");
+const secretKey = "Rahasia"
+const pool = require("../config.js");
 
 function authentication(req, res, next) {
+ const {access_token} = req.headers;
+ if(access_token) {
+   try {
+    const decoded = jwt.verify(access_token, secretKey);
+    const {id, email, gender, role} = decoded
+    const findUser = `
+    SELECT 
+        *
+    FROM users 
+    WHERE id = $1;
+    ` 
+     pool.query(findUser, [id], (err, result) => {
+        if(err) next(err);
 
+        if(result.rows.length === 0){
+        next({name: "ErrorNotFound"})
+        } else{
+            const user = result.rows[0]
+            
+            req.loggedUser =  {
+                id: user.id,
+                email: user.email,
+                gender: user.gender,
+                role: user.role
+            }
+            next();
+        }
+     })  
+} catch(err){
+        next({name: "JWTerror"})
+    }
+ }  
+ else {
+    next ({name: "unauthenticated"})
+ }
 }
 
 function authorization(req, res, next) {
 
+    const {username, email, id} = req.loggedUser;
+
+    if(username){
+        next();
+    } else{
+        next({ name: "unauthorized"})
+    }
 }
 
 module.exports = {
