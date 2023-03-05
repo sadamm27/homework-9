f7db147e52ba183a5cfe5c03f61d6f8ce0a9c2b1
Mochammad Sadam <122198036+sadamm27@users.noreply.github.com>
2023-03-05 14:50:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 const router = require("./routes/index.js")
 const errorHandler = require("./middlewares/errorhandler.js")
+const morgan = require("morgan")
 
+
+app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
